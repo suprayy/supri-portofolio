6ef4353555c6924e1dc566f8a08a5b031ced00e1
suprayy <supraayyy@gmail.com>
2024-02-14 23:45:38 +0700

    add: bagian footer

--- my-noted.jsx
+++ my-noted.jsx
@@ -2,4 +2,4 @@
 // -git add .
 // git commit -m "menu: ....."
 // git push -u origin main
-//1.2.00
+//1.41.00
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,5 @@
-import React from "react";
-
 const Footer = () => {
+  const year = new Date().getFullYear;
   return (
     <footer className="bg-[#12141e] pt-12">
       <div className="container">
@@ -22,6 +21,86 @@ const Footer = () => {
               accusamus ex atque quos aliquam maiores cupiditate, architecto,
               fugit aperiam voluptatum exercitationem libero.
             </p>
+            <div className="flex items-center gap-4 flex-wrap md:gap-8 mt-10">
+              <span className="text-gray-300 font-[600] text-[15px]">
+                Follow me:
+              </span>
+              <span className="w-[35px] h-[35px] bg-[#2b2d33] p-1 rounded-[50px] cursor-pointer text-center">
+                <a
+                  href="github"
+                  className="text-gray-300 font-[500] text-[18px]"
+                >
+                  <i className="ri-github-line"></i>
+                </a>
+              </span>
+              <span className="w-[35px] h-[35px] bg-[#2b2d33] p-1 rounded-[50px] cursor-pointer text-center">
+                <a
+                  href="github"
+                  className="text-gray-300 font-[500] text-[18px]"
+                >
+                  <i className="ri-github-line"></i>
+                </a>
+              </span>
+              <span className="w-[35px] h-[35px] bg-[#2b2d33] p-1 rounded-[50px] cursor-pointer text-center">
+                <a
+                  href="github"
+                  className="text-gray-300 font-[500] text-[18px]"
+                >
+                  <i className="ri-github-line"></i>
+                </a>
+              </span>
+              <span className="w-[35px] h-[35px] bg-[#2b2d33] p-1 rounded-[50px] cursor-pointer text-center">
+                <a
+                  href="github"
+                  className="text-gray-300 font-[500] text-[18px]"
+                >
+                  <i className="ri-github-line"></i>
+                </a>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div>
+          <ul className="flex items-center justify-center gap-10 mt-10">
+            <li className="text-gray-400 font-[600]">
+              <a href="#about">About</a>
+            </li>
+            <li className="text-gray-400 font-[600]">
+              <a href="#services">Services</a>
+            </li>
+            <li className="text-gray-400 font-[600]">
+              <a href="#portofolio">Portofolio</a>
+            </li>
+            <li className="text-gray-400 font-[600]">
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="bg-[#1b1e29] py-5 mt-14">
+        <div className="container">
+          <div className="flex items-center justify-center sm:justify-between">
+            <div className="hidden sm:block ">
+              <div className="flex items-center gap-[10px]">
+                <span className="w-[35px] h-[35px] rounded-full bg-[#2b2d33] text-white font-[500] text-[18px] flex items-center justify-center ">
+                  S
+                </span>
+                <div className="leading-[20px]">
+                  <h2 className="text-gray-200 font-[500] text-[18px]">
+                    Supriyono
+                  </h2>
+                  <p className="text-gray-400 text-[14px] font-[500]">
+                    Personal
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div>
+              <p className="text-gray-400 text-[14px]">
+                Copyright {year} developed by Supriyono - All right reserved.
+              </p>
+            </div>
           </div>
         </div>
       </div>
